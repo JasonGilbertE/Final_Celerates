b9062e2c77c8b6405a56a7c3a50617dfd38c0a40
Wildan Hidayatulloh <wildanhidayatulloh73@gmail.com>
2024-11-20 18:57:13 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductPage from "./pages/Product/ProductPage";
+import { UserProvider } from "./context/UserContext";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <>
+      <BrowserRouter>
+        <UserProvider>
+          <Routes>
+            <Route path="/" element={<ProductPage />} />
+            <Route path="/products/:slug" element={<ProductDetail />} />
+            <Route path="*" element={<h1 className="flex w-full h-full text-center items-center justify-center text-4xl text-pink-600">NOT FOUND PAGE.</h1>} />
+          </Routes>
+        </UserProvider>
+      </BrowserRouter>
+    </>
   );
-};
+}
 
 export default App;
