d10145bec662fcdc15abb299f876a3fd0688fdaa
anggiprtiwi12 <149142209+anggiprtiwi12@users.noreply.github.com>
2024-11-21 02:49:29 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './rating.css'
 import App from './App.jsx'
+import App from './Rating.jsx'
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
