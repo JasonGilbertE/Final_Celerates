fa3674871bdcd9442ce841e4630e2356bb9016d8
anggiprtiwi12 <149142209+anggiprtiwi12@users.noreply.github.com>
2024-11-21 03:25:20 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './rating.css'
+import './qris.css'
 import App from './App.jsx'
 import App from './Rating.jsx'
+import App from './Qris.jsx'
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
